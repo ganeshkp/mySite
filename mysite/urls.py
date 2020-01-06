4bc1ce78fa72eb12ae3b757372cf24a066c7bc3c
Ganesh <ganeshkumar.patil@gmail.com>
2020-01-06 18:45:39 +0000

    Committing modified files

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from django.contrib.auth import views
+from django.contrib.auth import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout', kwargs={'next_page': '/'}),
 ]
